81ece2e1c6b358cff1b9c8de76ce673933802a69
Joshua Almonte <jaa75@njit.edu>
2020-11-07 14:31:42 -0500

    Added Config Base for Later Development

--- Quintrix Training Project/src/com/my/testing/TheInternetTestBase.java
+++ Quintrix Training Project/src/com/my/testing/TheInternetTestBase.java
@@ -1,6 +1,9 @@
 package com.my.testing;
 
 import org.testng.annotations.BeforeTest;
+
+import java.net.URL;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
@@ -10,13 +13,33 @@ public class TheInternetTestBase {
 	
 	@BeforeTest
 	protected void beforeTest() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Josh\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		this.webDriver = new ChromeDriver();
-		this.baseUrl = "http://the-internet.herokuapp.com/";
+		LoadConfigurations();
 	}
 	
 	@AfterTest
 	protected void afterTest() {
 		this.webDriver.quit();
+	}
+	
+	
+	private void LoadConfigurations() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Josh\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		//URL url = ClassLoader.getSystemResource("chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", url.getFile());
+		this.webDriver = new ChromeDriver();
+		this.baseUrl = "http://the-internet.herokuapp.com/";
+/*
+		this.webDriver = new ChromeDriver();
+
+		HashMap<String, String> configs = null;
+
+		try {
+			configs = new ConfigurationReader().getPropertiesFromResourceFile("config.properties");
+		} catch (IOException e) {
+			throw new RuntimeException("Config file does not exist.");
+		}
+
+		this.baseUrl = configs.get(ConfigurationParameters.Url);
+*/	
 	}
 }
