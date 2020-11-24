285a170951b565971db87ade7d8b56746d872bb6
Joshua Almonte <jaa75@njit.edu>
2020-11-24 14:44:27 -0500

    Began setup of demoQA tests

--- practice/src/test/java/demoQA/tests/DemoQA.java
+++ practice/src/test/java/demoQA/tests/DemoQA.java
@@ -1,7 +1,35 @@
 package demoQA.tests;
 
+import org.testng.annotations.Test;
+
 import demoQA.foundation.DemoQATestBase;
 
 public class DemoQA extends DemoQATestBase{
 
+	/*
+	 * Test 1 (3 students) - Get the data from a CSV file. Ideally you will read the data into value objects (POJOs). src/test/java/dataDriven/CsvDriven.java
+Test 2 (3 students) - Take a collection of Data Provider Objects passed into the test. src/test/java/dataDriven/ParameterDriven.java
+Test 3 (3 students) - Get the data from a XML file. Ideally you will read the data into value objects (POJOs). src/test/java/dataDriven/XmlDriven.java
+Test 4 (3 students) - Get the data from a database. Use the DB Query to insert the data. src/test/java/dataDriven/DbDriven.java
+	 */
+	
+	@Test
+	public void getDataFromCSV() {
+		
+	}
+	
+	@Test
+	public void getDataFromDPO() {
+		
+	}
+	
+	@Test
+	public void getDataFromXML() {
+		
+	}
+	
+	@Test
+	public void getDataFromDB() {
+		
+	}
 }
